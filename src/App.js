7c43def49396587c877a7543d2cdbad0cc739cbd
izu99 <isuruumanga123@gmail.com>
2023-03-19 13:24:17 +0530

    to guide register

--- src/App.js
+++ src/App.js
@@ -2,7 +2,8 @@ import './App.css';
 // import Login from './Components/Login'
 // import Homepage from './Components/Homepage';
 // import Register from './Components/Register'
-import Search from './Components/Search';
+// import Search from './Components/Search';
+import GuideRegister from './Components/GuideRegister'
 
 function App() {
   return (
@@ -12,7 +13,9 @@ function App() {
 
     {/* < Register /> */}
 
-    <Search /> 
+    {/* <Search />  */}
+
+    <GuideRegister />
     </div>
   );
 }
