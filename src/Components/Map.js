9e7405fe1be6aabecd0174fa7265f8daed3f52d4
izu99 <isuruumanga123@gmail.com>
2023-03-23 18:46:15 +0530

    to destination details

--- src/Components/Map.js
+++ src/Components/Map.js
@@ -5,7 +5,7 @@ function MapLocation() {
     <iframe
       title="Google Map"
       width="1000"
-      height="600"
+      height="550"
       loading="lazy"
       allowFullScreen
       src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d31685.79037201192!2d79.8404254248674!3d6.923583972839667!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x3ae253d10f7a7003%3A0x320b2e4d32d3838d!2sColombo!5e0!3m2!1sen!2slk!4v1679572560472!5m2!1sen!2slk"
