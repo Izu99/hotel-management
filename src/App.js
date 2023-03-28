d20fb2b96bfc4763c1a4ccc2634ceeccf7abd4a0
sachinharshitha179@gmail.com <sachinharshitha179@gmail.com>
2023-03-28 11:24:25 +0530

    commit successfull

--- src/App.js
+++ src/App.js
@@ -1,7 +1,7 @@
-import "./App.css";
-// import Login from './Components/Login'
+import './App.css';
+//  import Login from './Components/Login'
 // import Homepage from './Components/Homepage';
-// import Register from './Components/Register'
+ import Register from './Components/Register'
 // import GuideSearch from './Components/GuideSearch';
 // import GuideRegister from './Components/GuideRegister'
 // import HotelSearch from './Components/HotelSearch'
@@ -10,9 +10,9 @@ import "./App.css";
 // import HotelBookingDetails from './Components/HotelBookingDetails'
 // import GuideBookingDetails from './Components/GuideBookingDetails';
 // import AdvertisingPayments from './Components/AdvertisingPayments'
-// import Payment from './Components/Payment'
+ //import Payment from './Components/Payment'
 // import Destinations from './Components/Destinations';
-// import DestinationDetails from './Components/DestinationDetails';
+//import DestinationDetails from './Components/DestinationDetails';
 // import MapLocation from './Components/Map'
 // import AddtoMap from './Components/AddtoMap';
 // import VehicleHomePage from './Components/VehicleHomePage';
@@ -25,12 +25,12 @@ import "./App.css";
 import AyurvedicFeedback from "./Components/AyurvedicFeedback";
 
 function App() {
-	return (
-		<div className='App'>
-			{/* < Login /> */}
-			{/* < Homepage /> */}
+  return (
+     <div className="App">     
+    {/* < Login /> */}
+    {/* < Homepage /> */}
 
-			{/* < Register /> */}
+      {/* < Register />  */}
 
 			{/* <GuideSearch />  */}
 
