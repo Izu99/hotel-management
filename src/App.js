25ad737f20ba9a8fea2c6ed779fd2a86b8d4a07f
sachinharshitha179@gmail.com <sachinharshitha179@gmail.com>
2023-03-28 12:02:25 +0530

    commit message

--- src/App.js
+++ src/App.js
@@ -1,7 +1,7 @@
 import './App.css';
 //  import Login from './Components/Login'
 // import Homepage from './Components/Homepage';
- import Register from './Components/Register'
+// import Register from './Components/Register'
 // import GuideSearch from './Components/GuideSearch';
 // import GuideRegister from './Components/GuideRegister'
 // import HotelSearch from './Components/HotelSearch'
