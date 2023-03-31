2f755b1c44f93ba011253054848e7bae3da2c575
izu99 <isuruumanga123@gmail.com>
2023-03-31 18:10:01 +0530

    homepage admin

--- src/Components/AddVehicle.js
+++ src/Components/AddVehicle.js
@@ -1,6 +1,6 @@
 import React from "react";
 import "../Styles/AddVehicle.css";
-import NavbarHome from "./NavBar_home";
+import NavbarHome from "./NavBar_homeAdmin";
 import Footer from "./Footer";
 
 function AddVehicle() {
