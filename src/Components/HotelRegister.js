83e11a71bdde8db8e49ed4d265ca3ca544d86ce2
sachinharshitha179@gmail.com <sachinharshitha179@gmail.com>
2023-03-31 19:26:52 +0530

    update sachin

--- src/Components/HotelRegister.js
+++ src/Components/HotelRegister.js
@@ -1,54 +1,151 @@
-import React, { useState } from 'react';
+import React, { Component } from 'react';
 import '../Styles/HotelRegister.css'
 import NavbarHome from './NavBar_home';
 import Footer from './Footer';
+import axios from "axios";
 
 
 
-function HotelRegister() {
+export default class HotelRegister extends Component {
+  constructor(props) {
+    super(props);
+    this.onChangehName = this.onChangehName.bind(this);
+    this.onChangeaddress = this.onChangeaddress.bind(this);
+    this.onChangepNumber = this.onChangepNumber.bind(this);
+    this.onChangeemail = this.onChangeemail.bind(this);
+    this.onChangepassword= this.onChangepassword.bind(this);
+    this.onChangecPassword = this.onChangecPassword.bind(this);
+    
+    this.onSubmit = this.onSubmit.bind(this);
 
+    this.state = {
+      hName: "",
+      address: "",
+      pNumber: "",
+      email: "",
+      password: "",
+      cPassword: ""
+     
+    };
+  }
+  onChangehName(e) {
+    this.setState({
+      hName: e.target.value,
+    });
+  }
+  onChangeaddress(e) {
+    this.setState({
+      address: e.target.value,
+    });
+  }
+  // eslint-disable-next-line no-dupe-class-members
+  onChangepNumber(e) {
+    this.setState({
+      pNumber: e.target.value,
+    });
+  }
+  onChangeemail(e) {
+    this.setState({
+      email: e.target.value,
+    });
+  }
+  onChangepassword(e) {
+    this.setState({
+      password: e.target.value,
+    });
+  }
+  onChangecPassword(e) {
+    this.setState({
+      cPassword: e.target.value,
+    });
+  }
+  
+  onSubmit(e) {
+    e.preventDefault();
+    const obj = {
+      hName: this.state.hName,
+      address: this.state.address,
+      pNumber: this.state.pNumber,
+      email: this.state.email,
+      password: this.state.password,
+      cPassword: this.state.cPassword,
+    };
+
+
+
+   
+        if (this.state.pNumber.length === 9) {
+          if (this.state.password.length >= 5) {
+            if (this.state.cPassword.length >= 5) {
+              axios.post("http://localhost:4000/hotel/hotelAdd", obj)
+                .then((res) => {
+                  alert("Tourist add Successfully");
+                  this.setState({
+                    hName: "",
+                    address: "",
+                    pNumber: "",
+                    email: "",
+                    password: "",
+                    cPassword: "",
+                  });
+                  console.log(res.data);
+                });
+              this.props.history.push("/Homepage");
+            } else {
+              alert("pleace enter valid confirm password.");
+            }
+          } else {
+            alert("pleace enter valid password.");
+          }
+        } else {
+          alert(
+            "Invalid phone number.. Pleace enter last 9 numbers of contact number."
+          );
+        }
+      
+    
+  }
+
+  render() {
     return (
       
       <div className='HotelRegister'>
         <NavbarHome />
-        <form>
+        <form onSubmit={this.onSubmit}>
           <div className="title">
             <p>Hotel Register</p>
           </div>
         <div className="basic-info">
           <label>Hotel Name</label>
-            <input type="text"/>
+            <input type="text" value={this.state.hName} onChange={this.onChangehName}/>
         </div>  
                 
         <div className="basic-info">
           <label>Address</label>
-            <textarea />
+          <input type="text" value={this.state.address} onChange={this.onChangeaddress}/>
         </div>
         
         <div className="basic-info">
           <label>Mobile Number</label>
-            <input type="text" />
+            <input type="text" value={this.state.pNumber} onChange={this.onChangepNumber}/>
         </div>
 
         <div className="basic-info">
           <label>Email Address</label>
-            <input type="email" />
+            <input type="email" value={this.state.email} onChange={this.onChangeemail}/>
         </div>
 
         <div className="basic-info">
           <label>Password</label>
-            <input type="password" />
+            <input type="password" value={this.state.password} onChange={this.onChangepassword}/>
         </div>
 
         <div className="basic-info">
-          <label>Re. Address</label>
-            <input type="password" />
+          <label>Confirm password</label>
+            <input type="password" value={this.state.cPassword} onChange={this.onChangecPassword}/>
         </div>
         
-        <div className="basic-info">
-          <label for="image-input">Hotel Image</label>
-          <input type="file" id="image-input" name="image" accept="image/*"/>
-        </div>       
+            
         <button className='submit' type="submit">Submit</button>
       </form>
       <Footer />
@@ -59,4 +156,4 @@ function HotelRegister() {
 };
 
 
-export default HotelRegister
+}
--- src/index.js
+++ src/index.js
@@ -1,17 +1,24 @@
 import React from 'react';
 import { BrowserRouter } from 'react-router-dom';
-import ReactDOM from 'react-dom/client';
+import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
+ReactDOM.render(
+  <BrowserRouter>
+          <App/>
+  </BrowserRouter>,
+  document.getElementById('root')
+);
 
-root.render(
-  <React.StrictMode>
-     <BrowserRouter>
-      <App />
-     </BrowserRouter>
-  </React.StrictMode>
+// const root = ReactDOM.createRoot(document.getElementById('root'));
+
+// root.render(
+//   <React.StrictMode>
+//      <BrowserRouter>
+//       <App />
+//      </BrowserRouter>
+//   </React.StrictMode>
  
-)
+// )
 
