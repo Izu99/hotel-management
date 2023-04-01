2c9b74309d396a31d2619ba5091f371fdebbb8eb
izu99 <isuruumanga123@gmail.com>
2023-04-01 12:52:32 +0530

    fix

--- src/Components/index.profile.js
+++ src/Components/index.profile.js
@@ -1,61 +1,59 @@
-import  React, {Component} from 'react';
+import React, { Component } from "react";
 import NavBarHome from "./NavBar_home";
 import Footer from "./Footer";
-import axios from 'axios';
+import axios from "axios";
 import TableRow from "./ProfileTableRow";
-import '../Styles/IndexProfile.css'
+import "../Styles/IndexProfile.css";
 
-    export default  class Index extends  Component{
+export default class Index extends Component {
+	constructor(props) {
+		super(props);
+		this.state = { hotel: [] };
+		this.state.Email = this.props.match.params.id;
 
-    constructor(props) {
-        super(props);
-        this.state = {hotel : []};
-        this.state.Email = this.props.match.params.id;
+		//const Email = this.props.match.params.id;
+	}
 
-        //const Email = this.props.match.params.id;
-    }
+	componentDidMount() {
+		//  alert('Username is ' +this.props.match.params.id);
+		axios
+			.get("http://localhost:4000/hotel/" + this.props.match.params.id)
+			.then((response) => {
+				alert("Pass una");
+				// alert('Data Tika :'+response.data)
+				this.setState({ hotel: response.data });
+			})
+			.catch(function (error) {
+				console.log(error);
+				alert("Pass unana");
+			});
+	}
+	tabRow() {
+		// eslint-disable-next-line react/jsx-no-undef
+		return <TableRow obj={this.state.hotel} />;
+	}
 
-    componentDidMount() {
-      //  alert('Username is ' +this.props.match.params.id);
-        axios.get('http://localhost:4000/hotel/'+this.props.match.params.id)
-            .then(response => {
-                 alert('Pass una')
-               // alert('Data Tika :'+response.data)
-                this.setState({hotel : response.data});
+	render() {
+		return (
+			<div className='indexProfile'>
+				<NavBarHome />
 
-            })
-            .catch(function (error){
-                console.log(error);
-               alert('Pass unana')
-            })
-    }
-    tabRow(){
-            // eslint-disable-next-line react/jsx-no-undef
-            return <TableRow obj={this.state.hotel}/>
-    }
+				<div className='profile-top'>
+					<div className='left-details'>
+						<h2>Profile Details</h2>
+						<div className='profile-image-container'>
+							<img
+								src='https://thereadtoday.com/wp-content/uploads/2020/12/solo-male-travel-800x445.jpg'
+								alt='Profile'
+								className='profile-image'
+							/>
+						</div>
+					</div>
+					<div className='right-details'>{this.tabRow()}</div>
+				</div>
 
-
-        render() {
-	return (
-		<div className='indexProfile'>
-			<NavBarHome />
-		
-            <div className='profile-top'>
-                   <div className='left-details'>
-                                           
-                     
-                   </div>
-                   <div className='right-details'>
-                    {this.tabRow()}
-                   </div>
-                  
-                    
-                </div>
-			
-			<Footer />
-		</div>
-	);
-    }
+				<Footer />
+			</div>
+		);
+	}
 }
-
-
--- src/Components/profileTableRow.js
+++ src/Components/profileTableRow.js
@@ -1,127 +1,58 @@
-import React, { Component } from "react";
-import { Link } from "react-router-dom";
+import React, {Component} from 'react';
+import {Link} from 'react-router-dom'
 import axios from "axios";
-import "../Styles/ProfileTableRow.css";
+
+
+
 
 export default class TableRow extends Component {
-	constructor(props) {
-		super(props);
-		this.state = {
-			hName: this.props.obj.hName,
-			address: this.props.obj.address,
-			pNumber: this.props.obj.pNumber,
-			email: this.props.obj.email,
-			password: this.props.obj.password,
-			cPassword: this.props.obj.cPassword,
-		};
-		this.delete = this.delete.bind(this);
-		this.handleChange = this.handleChange.bind(this);
-	}
+    constructor(props) {
+        super(props);
+        this.delete = this.delete.bind(this);
+    }
+    delete(){
+        axios.get('http://localhost:4000/hotel/hotelDelete/'+this.props.obj._id)
+            .then(this.setState({redirect: true}))
+            .catch(err => console.log(err))
+      
+      alert("Your Accout Successfully Deleted....")
+      window.location.replace('/signIn');
+    }
+    render() {
+        return (
+            <div className='pro-details'>
+                <table className="table table-striped">
+                   <tr>
+                      <td style={{fontWeight:'bold'}}>Hotel name</td>
+                      <td>{this.props.obj.hName}Hotel Name</td>
+                   </tr>
+                   <tr>
+                      <td style={{fontWeight:'bold'}}>address</td>
+                      <td>{this.props.obj.address}Address</td>
+                   </tr>
+                   <tr>
+                      <td style={{fontWeight:'bold'}}>Phone Nu</td>
+                      <td>{this.props.obj.pNumber}Phone Number</td>
+                   </tr>
+                   <tr>
+                      <td style={{fontWeight:'bold'}}>Email</td>
+                      <td>{this.props.obj.email}Email</td>
+                   </tr>
+                   <tr>
+                      <td style={{fontWeight:'bold'}}>Password</td>
+                      <td>{this.props.obj.password}Password</td>
+                   </tr>
+                   <tr>
+                      <td style={{fontWeight:'bold'}}>Password</td>
+                      <td>{this.props.obj.cPassword}Password</td>
+                   </tr>
+                   <tr className='profile-actions'>
+                      <td> <button><Link to={"/edit/"+this.props.obj._id} >Edit</Link></button></td>
+                      <td><button onClick={this.delete}>Delete Account</button></td>
+                   </tr>
+                </table>
+            </div>
 
-	handleChange(event) {
-		const { name, value } = event.target;
-		this.setState({ [name]: value });
-	}
-
-	delete() {
-		axios
-			.get("http://localhost:4000/hotel/hotelDelete/" + this.props.obj._id)
-			.then(this.setState({ redirect: true }))
-			.catch((err) => console.log(err));
-
-		alert("Your account was successfully deleted.");
-		window.location.replace("/signIn");
-	}
-
-	render() {
-		return (
-			<div className='profile-container'>
-				<div className='profile-header'>
-					<h2 className='profile-title'>Profile Details</h2>
-				</div>
-				<div className='profile-content'>
-					<div className='profile-image-container'>
-						<img
-							src='https://thereadtoday.com/wp-content/uploads/2020/12/solo-male-travel-800x445.jpg'
-							alt='Profile'
-							className='profile-image'
-						/>
-					</div>
-					<div className='profile-details-container'>
-						<div className='profile-details'>
-							<label htmlFor='hName'>Hotel Name</label>
-							<input
-								type='text'
-								id='hName'
-								name='hName'
-								value={this.state.hName}
-								onChange={this.handleChange}
-							/>
-						</div>
-						<div className='profile-details'>
-							<label htmlFor='address'>Address</label>
-							<input
-								type='text'
-								id='address'
-								name='address'
-								value={this.state.address}
-								onChange={this.handleChange}
-							/>
-						</div>
-						<div className='profile-details'>
-							<label htmlFor='pNumber'>Phone Number</label>
-							<input
-								type='text'
-								id='pNumber'
-								name='pNumber'
-								value={this.state.pNumber}
-								onChange={this.handleChange}
-							/>
-						</div>
-						<div className='profile-details'>
-							<label htmlFor='email'>Email</label>
-							<input
-								type='text'
-								id='email'
-								name='email'
-								value={this.state.email}
-								onChange={this.handleChange}
-							/>
-						</div>
-						<div className='profile-details'>
-							<label htmlFor='password'>Password</label>
-							<input
-								type='password'
-								id='password'
-								name='password'
-								value={this.state.password}
-								onChange={this.handleChange}
-							/>
-						</div>
-						<div className='profile-details'>
-							<label htmlFor='cPassword'>Confirm Password</label>
-							<input
-								type='password'
-								id='cPassword'
-								name='cPassword'
-								value={this.state.cPassword}
-								onChange={this.handleChange}
-							/>
-						</div>
-					</div>
-				</div>
-				<div className='profile-actions'>
-               <button>
-                  <Link to={"/edit/" + this.props.obj._id} className='btn btn-success'>
-						Edit
-					</Link>
-               </button>
-					
-					<button onClick={this.delete} className='btn btn-danger'>
-						Delete Account
-					</button>
-				</div>
-			</div>
-		);
-	}
+        );
+    }
 }
