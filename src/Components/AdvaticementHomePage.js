1d2867fbdbceb9d9a151a1490685bc2c541dfab9
Sachin Harshitha <39330996+thelllmike@users.noreply.github.com>
2023-04-26 00:06:14 +0530

    advatice

--- src/Components/AdvaticementHomePage.js
+++ src/Components/AdvaticementHomePage.js
@@ -1,116 +1,129 @@
-import React, {Component} from "react";
+import React, { Component } from "react";
 import image from "../images/profile-photo.png";
 import logo from "../images/logo (2).png";
 
 import "../Styles/NavBar_home.css";
-import '../Styles/SpecialPackages.css' 
+import Footer from "./Footer";
+import Slideshow2 from "./SlideShow";
+import "../Styles/OrderAndAdventureExperience.css";
 
-export default class AddvaticemnentHomepage extends Component {
-	render() {
-	return (
-		<div>
-			<nav>
-				<div className='logo'>
-					<a href='/'>
-						<img src={logo} alt='' srcset='' />
-					</a>
-				</div>
-				<ul className='nav-links'>
-					<li>
-						<a href={"/HotelHomepage/" + this.props.match.params.id}>hotel</a>
-					</li>
-					<li>
-						<a href={"/appointmentHomepage/" + this.props.match.params.id}>
-							Appointment
-						</a>
-					</li>
-					<li>
-						<a href={"/TourguideHomepage/" + this.props.match.params.id}>
-							Guide
-						</a>
-					</li>
-					<li>
-						<a href='/Payment'>Payment</a>
-					</li>
-					<li>
-						<a href={"/RoomHomePage/" + this.props.match.params.id}>Room</a>
-					</li>
-					<li>
-						<a href={"/VehicleHomePage/" + this.props.match.params.id}>
-							Vehicle
-						</a>
-					</li>
-					<li>
-						<a
-							href={
-								"/AyurvedicTreatmentHomePage/" + this.props.match.params.id
-							}>
-							Treatement
-						</a>
-					</li>
-					<li>
-						<a href={"/destinations/" + this.props.match.params.id}>
-							Destination
-						</a>
-					</li>
-					<li>
-						<a href={"/ExperienceHomepage/" + this.props.match.params.id}>
-							Experience
-						</a>
-					</li>
-				</ul>
-				<div className='profile'>
-						<a href={"/index/" + this.props.match.params.id}>
-							{/* Experience */}
-							<img src={image} alt='' srcset='' />
-						</a>
-						<i class='fa-solid fa-ellipsis-vertical'></i>
-					</div>
-			</nav>
-			<div className="specialPackages">
-			<h1>Tour Packages</h1>
-			<div className='packages'>
-				<div className='package'>
-					<h3>Budget Explorer</h3>
-					<h4>$1000</h4>
-					<ul>
-						<li>Basic hotel accommodations</li>
-						<li>Public transportation</li>
-						<li>Self-guided tours</li>
-						<li>Local food experiences</li>
-					</ul>
-					<button type="submit">Buy</button>
-				</div>
-				<div className='package'>
-					<h3>Cultural Immersion</h3>
-					<h4>$2500</h4>
-					<ul>
-						<li>Mid-range hotel accommodations</li>
-						<li>Guided tours to cultural and historical sites</li>
-						<li>Local cuisine and cooking classes</li>
-						<li>Authentic cultural experiences</li>
-					</ul>
-					<button type="submit">Buy</button>
-				</div>
-				<div className='package'>
-					<h3>Luxury Explorer</h3>
-					<h4>$5000</h4>
-					<ul>
-						<li>Luxury hotel accommodations</li>
-						<li>Private transportation</li>
-						<li>Private guided tours</li>
-						<li>Fine dining and wine experiences</li>
-					</ul>
-					{/* <button type="submit">Buy</button> */}
-					<button className='orderButton'>
-									<a href={"/MyAdvaticementOders/" + this.props.match.params.id}>
-									Buy
-									</a>
-								</button>
-				</div>
-			</div>
-		</div>
-		</div>
-	);
-}
+// import image from "../images/profile-photo.png";
+import img1 from "../images/Travel -01.png";
+import img2 from "../images/Travel -02.png";
+import img3 from "../images/Travel -03.png";
+import img4 from "../images/Travel -04.png";
+import img5 from "../images/Travel -05.png";
+import img6 from "../images/Travel -06.png";
+
+
+
+export default class Experience extends Component {
+  render() {
+    const adventures = [
+      { id: 1, title: "Travel Agency", select: "Description 1", img: img5 },
+      { id: 2, title: "Adventure Company", select: "Description 2", img: img2 },
+      { id: 3, title: "Travel Agency", select: "Description 3", img: img3 },
+      { id: 4, title: "Travel Agency", select: "Description 1", img: img4 },
+      {
+        id: 5,
+        title: "Travel Agency",
+        description: "Description 2",
+        img: img5,
+      },
+      { id: 6, title: "Vacancies", description: "Description 2", img: img6 },
+      { id: 7, title: "Travel Agency", select: "Description 1", img: img4 },
+      { id: 8, title: "Travel Agency", description: "Description 2", img: img5 },
+      { id: 9, title: "Travel Agency", description: "Description 2", img: img3 },
+    ];
+    return (
+      <div className="OrderAndAdventureExperience">
+        <nav>
+          <div className="logo">
+            <a href="/">
+              <img src={logo} alt="" srcset="" />
+            </a>
+          </div>
+          <ul className="nav-links">
+            <li>
+              <a href={"/HotelHomepage/" + this.props.match.params.id}>hotel</a>
+            </li>
+            <li>
+							<a href={"/AdvaticementHomePage/" + this.props.match.params.id}>
+								Advatice
+							</a>
+						</li>
+            <li>
+              <a href={"/ApppointmentHomepage/" + this.props.match.params.id}>
+                Appointment
+              </a>
+            </li>
+            <li>
+              <a href={"/TourguideHomepage/" + this.props.match.params.id}>
+                Guide
+              </a>
+            </li>
+            
+            <li>
+              <a href={"/RoomHomePage/" + this.props.match.params.id}>Room</a>
+            </li>
+            <li>
+              <a href={"/VehicleHomePage/" + this.props.match.params.id}>
+                Vehicle
+              </a>
+            </li>
+            <li>
+              <a
+                href={
+                  "/AyurvedicTreatmentHomePage/" + this.props.match.params.id
+                }
+              >
+                Treatement
+              </a>
+            </li>
+            <li>
+              <a href={"/destinations/" + this.props.match.params.id}>
+                Destination
+              </a>
+            </li>
+            <li>
+              <a href={"/ExperienceHomepage/" + this.props.match.params.id}>
+                Experience
+              </a>
+            </li>
+          </ul>
+          <div className="profile">
+            <a href={"/index/" + this.props.match.params.id}>
+              {/* Experience */}
+              <img src={image} alt="" srcset="" />
+            </a>
+            <i class="fa-solid fa-ellipsis-vertical"></i>
+          </div>{" "}
+        </nav>
+        <Slideshow2 />
+
+        <div className="adventureCardContainer">
+          {adventures.map((adventure) => (
+            <div key={adventure.id} className="adventureCard">
+              <div className="top">
+                <img src={adventure.img} alt="" />
+              </div>
+              <div className="bottom">
+                <p className="title">{adventure.title}</p>
+                <button className="orderButton">
+                  <a href={"/MyAdvaticementOders/" + this.props.match.params.id}>
+                    Buy
+                  </a>
+                </button>
+              </div>
+            </div>
+          ))}
+        </div>
+
+        {/* <AdventureAndExperienceCard /> */}
+        {/* </div> */}
+
+        <Footer />
+      </div>
+    );
+  }
 }
